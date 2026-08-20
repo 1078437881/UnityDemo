f40db12a568766bbe1551d614bdea686f1f028d4
wanyibo <yb_wan@droidhang.com>
2026-08-20 16:12:15 +0800

    [add]demo测试
    [add]测试

--- Demo/Assets/Scripts/DemoTest.cs
+++ Demo/Assets/Scripts/DemoTest.cs
@@ -1,32 +1,45 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DemoTest : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]
-    private int testValue;
-    
+    [SerializeField] private int testValue;
+
     public int testValue1;
-    
+
+    public TextMeshProUGUI textShow;
+    public TMP_InputField inputField;
+
     [System.Serializable]
     public class CustomDataType
     {
         public int nestValue;
     }
-    
+
     public CustomDataType testValue2;
-    
+
     void Start()
     {
-        Debug.Log("Hello World! Start"); 
+        inputField.onValueChanged.AddListener((content => 
+            textShow.text = content));
+        inputField.onValidateInput = delegate(string input, int charIndex, char addedChar)
+        {
+            if (addedChar == 'b')
+            {
+                addedChar = '*';
+            }
+
+            return addedChar;
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
